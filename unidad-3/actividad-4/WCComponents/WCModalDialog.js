83b6419fb189f7d7f6ab738c22f387dd176cee84
Matias <matiasdigialleonardo@gmail.com>
2025-07-26 22:43:31 -0300

    Added exercise 4 from unit 3

---
+++ unidad-3/actividad-4/WCComponents/WCModalDialog.js
@@ -0,0 +1,85 @@
+class WCModalDialog extends HTMLElement
+{
+	constructor()
+	{
+		super();
+
+		// Should modal be just 'this', as in, the class itself?
+		this.modal = document.createElement('div');
+		this.modal.classList.add('w3-modal');
+
+		let modalContent = document.createElement('div');
+		modalContent.classList.add('w3-modal-content');
+
+		let modalContainer = document.createElement('div');
+		modalContainer.classList.add('w3-container');
+
+		this.closeModalBtn = document.createElement('span');
+		this.closeModalBtn.classList.add('w3-button', 'w3-display-topright');
+		this.closeModalBtn.textContent = 'x';
+
+		let modalFirstText =  document.createElement('p');
+		modalFirstText.textContent = 'Some text. Some text. Some text.'
+
+		let modalSecondText = document.createElement('p');
+		modalSecondText.textContent = 'Some text. Some text. Some text.'
+
+		modalContainer.appendChild(this.closeModalBtn);
+		modalContainer.appendChild(modalFirstText);
+		modalContainer.appendChild(modalSecondText);
+
+		modalContent.appendChild(modalContainer);
+
+		this.modal.appendChild(modalContent);
+	}
+
+	onCloseModalDialogBtnClick(event)
+	{
+		this.modal.style.display = 'none';
+	}
+
+
+	connectedCallback()
+	{
+		//Se va a ejecutar siempre cuando el elemento es insertado en el DOM
+		//DOM = Arbol de elementos HTML ya como instancias (objetos) de JS.
+
+		this.appendChild(this.modal);
+
+		this.closeModalBtn.onclick = this.onCloseModalDialogBtnClick.bind(this);
+
+	}
+
+	disconnectedCallback()
+	{
+		//Se va a ejecutar siempre que se quite el elemento del documento
+	}
+
+	adoptedCallback()
+	{
+		//Se va a ejecutar siempre que el elemento se cambie de documento.
+	}
+
+	connectedMoveCallback()
+	{
+		//Se ejecuta cuando se mueve el elemento dentro del DOM
+	}
+
+	static get observableAttributes()
+	{
+		//Solo para publicar cuáles son los atributos que tendría disponible el webcomponent
+		//Si es utilizado a través de código HTML
+		//Ejemplo: <mi-elemento sabor="acido"> </mi-elemento>
+
+		return ['sabor']
+	}
+
+	attributeChangedCallback(attr, newvalue, oldvalue )
+	{
+		//Manejador de cambios de los valores de los atributos personalizados
+	}
+}
+
+customElements.define('x-wcmodaldialog', WCModalDialog );
+
+export { WCModalDialog }
---
+++ unidad-3/actividad-4/main.js
@@ -0,0 +1,25 @@
+import { WCModalDialog } from './WCComponents/WCModalDialog.js';
+
+function main()
+{
+	let wcmodaldialog = new WCModalDialog();
+
+	let modalDialogWCtitle = document.createElement('h2');
+	modalDialogWCtitle.textContent = 'W3.CSS Modal';
+
+	let openModalDialogWCbtn = document.createElement('button');
+	openModalDialogWCbtn.textContent = 'Open Modal';
+
+	document.body.append(modalDialogWCtitle);
+	document.body.append(openModalDialogWCbtn);
+
+	document.body.append(wcmodaldialog);
+
+	openModalDialogWCbtn.onclick = function()
+	{
+		wcmodaldialog.modal.style.display = 'block';
+	}
+
+}
+
+window.onload = main;
